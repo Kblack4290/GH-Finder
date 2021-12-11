95ed24e19ec1016950425cb4b60e3af51f72bd96
Keith Laptop <Keithblack4290@gmail.com>
2021-12-11 13:10:56 -0700

    moved search func from app to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -22,6 +22,20 @@ const GithubState = props => {
 
     //Search users
 
+    const searchUsers = async text => {
+
+        setLoading();
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+
+
+        dispatch({
+            type: SEARCH_USERS,
+            payload: res.data.items
+        })
+
+    };
+
     //get users
 
     //get repos
@@ -29,12 +43,13 @@ const GithubState = props => {
     //clear users
 
     //set loading 
-
+    const setLoading = () => dispatch({ type: SET_LOADING })
     return <GithubContext.Provider value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
     }}>
 
         {props.children}
